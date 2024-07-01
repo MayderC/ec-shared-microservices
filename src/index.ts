46fa4279247d2e02fa2d5d6bb2efc4daa314feb9
Mayder C <cortesdavid381@gmail.com>
2024-07-01 02:52:39 -0600

    add index ts

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+export {
+    IAuth,
+    IAuthDocument,
+    IAuthPayload,
+    IAuthUser,
+    IAuthResponse,
+    IAuthBuyerMessageDetails,
+    IEmailMessageDetails,
+    IReduxAuthPayload,
+    IForgotPassword,
+    IReduxAddAuthUser,
+    IReduxLogout,
+    ISignInPayload,
+    ISignUpPayload,
+    IResetPassword,
+} from "./interfaces/auth.interface"
+
+export {
+    IReduxBuyer,
+    IBuyerDocument,
+} from "./interfaces/buyer.interface"
+
+export  {
+    IChatBoxProps,
+    IChatBuyerProps,
+    IChatMessageProps,
+    IChatSellerProps,
+    IConversationDocument,
+    IMessageDetails,
+    IMessageDocument
+} from "./interfaces/chat.interface"
+
+export {
+    IEmailLocals
+} from "./interfaces/email.interface"
+
+export {
+    ISellerGig,
+    GigType,
+    ICreateGig,
+    IGigCardItems,
+    IGigContext,
+    IGigInfo,
+    IGigsProps,
+    IGigTopProps,
+    IGigViewReviewsProps,
+    ISelectedBudget
+} from "./interfaces/gig.interface"
+
+export {
+    IOffer,
+    IDeliveredWork,
+    IExtendedDelivery,
+    IOrderDocument,
+    IOrderEvents,
+    IOrderMessage,
+    IOrderNotifcation,
+    IOrderReview
+} from "./interfaces/order.interface"
+
+export {
+    IRatingCategories,
+    IRatingCategoryItem,
+    IRatingTypes,
+    IReviewDocument,
+    IReviewMessageDetails
+} from "./interfaces/review.interface"
+
+export {
+    IHitsTotal,
+    IQueryList,
+    IPaginateProps,
+    ITerm,
+    IQueryString,
+    ISearchResult
+} from "./interfaces/search.interface"
+
+export {
+    ISellerDocument,
+    IEducation,
+    ICertificate,
+    IExperience,
+    ILanguage,
+    SellerType
+} from "./interfaces/seller.interface"
